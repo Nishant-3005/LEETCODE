a08cd843ace1d40274441892cce34a60cb08f50c
Nishant-3005 <300305nishant@gmail.com>
2026-08-18 16:58:26 +0530

    Time: 0 ms (100%), Space: 9 MB (55.1%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int open=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(open>0){
+                    ans+=s[i];
+                }
+                open++;
+            }
+            else{
+                open--;
+            if(open>0){
+                ans+=s[i];
+            }}}return ans;
+}};
